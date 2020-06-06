b1ac5f741ac3045ddf0d66593beed7485af1bfd3
Poga Kuofie <mail@pogakuofie.com>
2020-06-06 13:24:36 +0000

    fix: test

--- src/containers/dashboard/tests/dashboard.test.js
+++ src/containers/dashboard/tests/dashboard.test.js
@@ -1,18 +1,10 @@
 /* eslint-disable no-undef */
-// import React from 'react'
-// import { render } from '@testing-library/react'
-// import Dashboard from '../Dashboard'
+import React from 'react'
+import { render } from '@testing-library/react'
+import Phone from '../../../components/phone'
 
-// test('renders main msg', () => {
-//   const { getByText } = render(<Dashboard />)
-//   const linkElement = getByText(/Send SMS for Free!/i)
-//   expect(linkElement).toBeInTheDocument()
-// })
-
-// test('renders description', () => {
-//   const { getByText } = render(<Dashboard />)
-//   const linkElement = getByText(
-//     /Send, save and manage your contacts, all in one place./i
-//   )
-//   expect(linkElement).toBeInTheDocument()
-// })
+test('renders description', () => {
+  const { getByText } = render(<Phone />)
+  const linkElement = getByText(/Enter your Phone Number/i)
+  expect(linkElement).toBeInTheDocument()
+})
