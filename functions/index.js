279a003df0ed7a81181be171d0dc9f7d8a6d73af
Poga Kuofie <mail@pogakuofie.com>
2020-06-29 13:50:40 +0000

    feat: setup express

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,15 @@ const functions = require('firebase-functions')
 const express = require('express')
 
 const app = express()
-const cors = require('cors')({
-  origin: '*',
+const router = express.Router()
+
+router.post('/register', (req, res) => {
+  res.send('helrlo')
+})
+router.post('/verify', (req, res) => {
+  res.send('helvlo')
 })
 
-app.use('*', cors)
+app.use('/api', router)
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express on Firebase!')
-})
-
-exports.api = functions.https.onRequest(app)
-
-exports.api = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
-})
+exports.fns = functions.https.onRequest(app)
